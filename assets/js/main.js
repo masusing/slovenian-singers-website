b3ac9de170e4aa3f9dc0c405d856f8d00fabf8a0
masusing <73908033+masusing@users.noreply.github.com>
2020-11-11 22:06:58 +0100

    Alessandro blog collapsing stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,135 +4,145 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function ($) {
+(function($) {
 
-	var $window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$footer = $('#footer'),
-		$main = $('#main'),
-		settings = {
+  var $window = $(window),
+    $body = $('body'),
+    $header = $('#header'),
+    $footer = $('#footer'),
+    $main = $('#main'),
+    settings = {
 
-			// Parallax background effect?
-			parallax: true,
+      // Parallax background effect?
+      parallax: true,
 
-			// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 20
+      // Parallax factor (lower = more intense, higher = less intense).
+      parallaxFactor: 20
 
-		};
+    };
 
-	// Breakpoints.
-	breakpoints({
-		xlarge: ['1281px', '1800px'],
-		large: ['981px', '1280px'],
-		medium: ['737px', '980px'],
-		small: ['481px', '736px'],
-		xsmall: [null, '480px'],
-	});
+  // Breakpoints.
+  breakpoints({
+    xlarge: ['1281px', '1800px'],
+    large: ['981px', '1280px'],
+    medium: ['737px', '980px'],
+    small: ['481px', '736px'],
+    xsmall: [null, '480px'],
+  });
 
-	// Play initial animations on page load.
-	$window.on('load', function () {
-		window.setTimeout(function () {
-			$body.removeClass('is-preload');
-		}, 100);
-	});
+  // Play initial animations on page load.
+  $window.on('load', function() {
+    window.setTimeout(function() {
+      $body.removeClass('is-preload');
+    }, 100);
+  });
 
-	// Touch?
-	if (browser.mobile) {
+  // Touch?
+  if (browser.mobile) {
 
-		// Turn on touch mode.
-		$body.addClass('is-touch');
+    // Turn on touch mode.
+    $body.addClass('is-touch');
 
-		// Height fix (mostly for iOS).
-		window.setTimeout(function () {
-			$window.scrollTop($window.scrollTop() + 1);
-		}, 0);
+    // Height fix (mostly for iOS).
+    window.setTimeout(function() {
+      $window.scrollTop($window.scrollTop() + 1);
+    }, 0);
 
-	}
+  }
 
-	// Footer.
-	// breakpoints.on('<=medium', function() {
-	// 	$footer.insertAfter($main);
-	// });
+  // Footer.
+  // breakpoints.on('<=medium', function() {
+  // 	$footer.insertAfter($main);
+  // });
 
-	// breakpoints.on('>medium', function() {
-	// 	$footer.appendTo($header);
-	// });
+  // breakpoints.on('>medium', function() {
+  // 	$footer.appendTo($header);
+  // });
 
-	// Header.
+  // Header.
 
-	// Parallax background.
+  // Parallax background.
 
-	// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-	if (browser.name == 'ie'
-		|| browser.mobile)
-		settings.parallax = false;
+  // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+  if (browser.name == 'ie' ||
+    browser.mobile)
+    settings.parallax = false;
 
-	if (settings.parallax) {
+  if (settings.parallax) {
 
-		breakpoints.on('<=medium', function () {
+    breakpoints.on('<=medium', function() {
 
-			$window.off('scroll.strata_parallax');
-			$header.css('background-position', '');
+      $window.off('scroll.strata_parallax');
+      $header.css('background-position', '');
 
-		});
+    });
 
-		breakpoints.on('>medium', function () {
+    breakpoints.on('>medium', function() {
 
-			$header.css('background-position', 'left 0px');
+      $header.css('background-position', 'left 0px');
 
-			$window.on('scroll.strata_parallax', function () {
-				$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-			});
+      $window.on('scroll.strata_parallax', function() {
+        $header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+      });
 
-		});
+    });
 
-		$window.on('load', function () {
-			$window.triggerHandler('scroll');
-		});
+    $window.on('load', function() {
+      $window.triggerHandler('scroll');
+    });
 
-	}
+  }
 
-	// Main Sections: Two.
+  // Main Sections: Two.
 
-	// isotope
-	var $grid = $('.isotope-grid').isotope({
-		// options
-		itemSelector: '.isotope-grid-item',
-		layoutMode: 'fitRows'
-	});
+  // isotope
+  var $grid = $('.isotope-grid').isotope({
+    // options
+    itemSelector: '.isotope-grid-item',
+    layoutMode: 'fitRows'
+  });
 
-	$('.filters-button-group').on('click', 'button', function () {
-		var filterValue = $(this).attr('data-filter');
-		$grid.isotope({ filter: filterValue });
-	});
-	// change is-checked class on buttons
-	$('.button-group').each(function (i, buttonGroup) {
-		var $buttonGroup = $(buttonGroup);
-		$buttonGroup.on('click', 'button', function () {
-			$buttonGroup.find('.is-checked').removeClass('is-checked');
-			$(this).addClass('is-checked');
-		});
-	});
-	// end isotope
+  $('.filters-button-group').on('click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each(function(i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $(this).addClass('is-checked');
+    });
+  });
+  // end isotope
 
-	// Lightbox gallery.
-	$window.on('load', function () {
+  // Lightbox gallery.
+  $window.on('load', function() {
 
-		$('#two').poptrox({
-			caption: function ($a) { return $a.next('h3').text(); },
-			overlayColor: '#2c2c2c',
-			overlayOpacity: 0.85,
-			popupCloserText: '',
-			popupLoaderText: '',
-			selector: '.work-item a.image',
-			usePopupCaption: true,
-			usePopupDefaultStyling: false,
-			usePopupEasyClose: false,
-			usePopupNav: true,
-			windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-		});
+    $('#two').poptrox({
+      caption: function($a) { return $a.next('h3').text(); },
+      overlayColor: '#2c2c2c',
+      overlayOpacity: 0.85,
+      popupCloserText: '',
+      popupLoaderText: '',
+      selector: '.work-item a.image',
+      usePopupCaption: true,
+      usePopupDefaultStyling: false,
+      usePopupEasyClose: false,
+      usePopupNav: true,
+      windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+    });
 
-	});
+  });
+
+  $(".collapsible-button").on("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  })
 
 })(jQuery);
