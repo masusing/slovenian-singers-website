831e9c407b1aa966bdb4da2fc5d1faf1085e00a9
Alessandro Macagno <ale.macagno@hotmail.it>
2021-09-11 13:34:50 +0200

    other pages plus isotope fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,24 +96,26 @@
   // Main Sections: Two.
 
   // isotope
-  var $grid = $('.isotope-grid').isotope({
-    // options
-    itemSelector: '.isotope-grid-item',
-    layoutMode: 'fitRows'
-  });
-
-  $('.filters-button-group').on('click', 'button', function () {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
-  // change is-checked class on buttons
-  $('.button-group').each(function (i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'button', function () {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $(this).addClass('is-checked');
+  if ($('.isotope-grid').length > 0) {
+    var $grid = $('.isotope-grid').isotope({
+      // options
+      itemSelector: '.isotope-grid-item',
+      layoutMode: 'fitRows'
     });
-  });
+
+    $('.filters-button-group').on('click', 'button', function () {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.button-group').each(function (i, buttonGroup) {
+      var $buttonGroup = $(buttonGroup);
+      $buttonGroup.on('click', 'button', function () {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+      });
+    });
+  }
   // end isotope
 
   // Lightbox gallery.
